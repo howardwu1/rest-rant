472133f74067250828bcf0cc8f11d8d67b06d8d7
danazou <dana.sou@gmail.com>
2022-11-14 14:12:42 -0800

    Jazzed up form page

--- views/show.jsx
+++ views/show.jsx
@@ -1,14 +1,15 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
           </main>
         </Def>
     )
 }
 
 module.exports = show
+
