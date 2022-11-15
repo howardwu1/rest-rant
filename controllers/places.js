5c3e1430488b250515de926a146616ec974b8e4e
danazou <dana.sou@gmail.com>
2022-11-15 12:18:27 -0800

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const app = express()
-const router = express.Router()
-const places = require('../models/places.js')
+const express = require('express');
+const app = express();
+const router = express.Router();
+const places = require('../models/places.js');
 
 // GET /places
-router.get('/', (req, res) => {
-  
+router.get('/', (req, res) => { 
 res.render('places/index', {places})
-}) 
+});
 
 //GET/places/new
 router.get('/new', (req, res) => {
-
   res.render('places/new')
-})
+});
 
 //POST/places
 router.post('/', (req, res) => {
-
   if (!req.body.pic) {
     // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
@@ -29,10 +26,10 @@ router.post('/', (req, res) => {
     req.body.state = 'USA'
   }
   places.push(req.body)
-  res.send('POST /places')
-})
+  res.redirect('/places');
+});
 
-//get(show)
+//show
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -45,8 +42,9 @@ router.get('/:id', (req, res) => {
     res.render('places/show', { place: places[id], id})
 
   }
-})
+});
 
+//delete
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -59,9 +57,49 @@ router.delete('/:id', (req, res) => {
     places.splice(id, 1)
     res.redirect('/places')
   }
-})
+});
 
+//edit
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+});
 
+//put
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+});
 
 
 
