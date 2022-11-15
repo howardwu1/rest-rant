304ad923bbce06925b4e04842553d95b6b126538
danazou <dana.sou@gmail.com>
2022-11-15 12:08:52 -0800

    Added the edit form

--- views/show.jsx
+++ views/show.jsx
@@ -7,22 +7,17 @@ function show (data) {
           <main>
             <h1>{ data.place.name }</h1>
             <h2>Rating</h2>
-              <p>Not Yet Rated</p>
+              <p>Not Rated</p>
             <h2>Comments</h2>
               <p>No Comments</p>
 
-              <a href={`/placecs/${data.id}/edit`} className="btn btn-warning"> 
-                  Edit
-                </a>  
+              <a href={`/placecs/${data.id}/edit`} className="btn btn-warning"> Edit</a>  
               <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">
-                  Delete
-                 </button>
-              </form>     
-
+                <button type="submit" className="btn btn-danger">Delete</button>
+              </form>    
           </main>
         </Def>
-    )
+    );
 }
 
 
