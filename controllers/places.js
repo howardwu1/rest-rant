6c201335c2c32af4987d9a52a1b9316a04f8aea0
danazou <dana.sou@gmail.com>
2022-11-22 21:24:30 -0800

    homework Part 10 complete with comment forms

--- controllers/places.js
+++ controllers/places.js
@@ -34,7 +34,9 @@ router.get('/new', (req, res) => {
 //show
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -42,6 +44,7 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
+
 
 //update
 router.put('/:id', (req, res) => {
@@ -67,4 +70,28 @@ router.post('/:id/rant', (req, res) => {
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
 })
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+
+
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻 '}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
   return (
     <Def>
       <main>
@@ -21,11 +40,11 @@ function show(data) {
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
 
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
               Edit
             </a>
 
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
@@ -34,7 +53,32 @@ function show(data) {
           </div>
         </div>
         <h2>Comments</h2>
-          <p>No Comments</p>
+  
+        {comments}
+        <h2>Got your own Rant or Rave?</h2>
+        <form method="POST" action={`${data.place.id}/comment`}>
+        <div className="form-group">
+                    <label htmlFor="content">Content</label>
+                    <input className="form-control" id="content" name="content" />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="author">Author</label>
+                    <input className="form-control" id="author" name="author" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="stars">Star Rating</label>
+                    <input type="number" className="form-control" id="stars" name="stars" step={0.5} value={5} />
+                 </div>
+                 <div className="form-group">
+                    <label htmlFor="rant">Rant?</label>
+                    <input id="rant" name="rant" type="checkbox" value={false}/>
+                </div>
+        
+                
+
+                    <input className="btn btn-primary" type="submit" value="Add Comment" />
+            </form>
       </main>
     </Def>
   );
