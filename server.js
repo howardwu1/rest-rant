398560f30f51e3cad37662048c1181cb592be8b8
howard23 <howard@radiusxr.com>
2022-12-04 20:16:03 -0600

    changed the ordering of jsx engine and set statements

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const methodOverride = require("method-override");
 
 // Express Settings
 app.set("views", __dirname + "/views");
-app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+app.set("view engine", "jsx");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
