fa5a80097f73c802d49949642873c0b0da36ea5d
howard23 <howard@radiusxr.com>
2022-12-04 19:01:20 -0600

    removed app.static from server.js as per instructions

--- server.js
+++ server.js
@@ -1,40 +1,31 @@
 // Modules and Globals
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 //const router = require('./controllers/places');
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 
 // Express Settings
 //app.set('views', __dirname + '/views');
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
-app.use(express.static('public'));
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-
+app.use(methodOverride("_method"));
 
 //app.use('/places', router)
 
 // Controllers & Routes
-app.use('/places', require('./controllers/places'));
+app.use("/places", require("./controllers/places"));
 
-
-app.get('/', (req, res) => {
-    res.render('home')
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get('*', (req, res) => {
-    res.render('error404')
+app.get("*", (req, res) => {
+  res.render("error404");
 });
 
 // Listen for Connections
 app.listen(process.env.PORT);
 
-module.exports= app;
-
-
-
-
-
-
+module.exports = app;
