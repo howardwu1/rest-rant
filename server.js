04f2e0aad811c90d30566db64a8e2ce5d56b9824
howard23 <howard@radiusxr.com>
2022-12-04 19:28:34 -0600

    uncommentd out teh app.set views line

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 const methodOverride = require("method-override");
 
 // Express Settings
-//app.set('views', __dirname + '/views');
+app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
